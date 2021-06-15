08e604a58e0442fc1bf2c41b71b534ad7b364816
Aniket Inamdar <aniket.1020@gmail.com>
2021-06-15 14:44:52 +0530

    Ruby  call

--- Workers/jobrun.rb
+++ Workers/jobrun.rb
@@ -8,6 +8,8 @@ COMMAND = ARGV[0]
 # Process::Sys.setgid(TARGET_GID)
 # Process::Sys.seteuid(TARGET_UID)
 
+$SAFE = 4
+
 Process::setrlimit(Process::RLIMIT_DATA, 1024 * 1024 * 256 * 4)   # 1GB Memory alloc
 Process::setrlimit(Process::RLIMIT_CPU, 5, 10)                    # 5s Runtime
 
